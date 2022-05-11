e9d5669a684dc81fe663fdfdc199659d103b8922
Klemen <roxor1992@gmail.com>
2022-05-11 16:59:28 +0200

    Added some fixes

--- gdg.cpp
+++ gdg.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 
 using std::abs;
-using std::find;
 using std::make_tuple;
 
 string GDG::info() {
@@ -16,39 +15,81 @@ string GDG::sinfo() {
 
 tuple<vector<unsigned int>, vector<vector<unsigned int>>> GDG::run(TestFuncBounds* ifunc) {
     initRun(ifunc);
-    auto seps = vector<uint>();
-    auto allgroups = vector<vector<uint>>();
-    auto dims = vector<uint>(func->dim);
-    for (uint i = 0; i < func->dim; i++) dims[i] = i;
-    auto p1 = new double[func->dim], p2 = new double[func->dim];
-    while (dims.size() > 0) {
-        auto i = dims[0];
-        auto group = vector<uint>(1, i);
-        for (auto j : dims) if (i != j) {
-            for (uint k = 0; k < func->dim; k++) p1[k] = p2[k] = func->x_bound_min[k];
-            p2[i] = func->x_bound_max[i];
-            auto y1 = eval(p1), y2 = eval(p2);
-            auto delta1 = y1 - y2;
-            p1[j] = p2[j] = (func->x_bound_min[j] + func->x_bound_max[j]) / 2;
-            auto y3 = eval(p1), y4 = eval(p2);
-            auto delta2 = y3 - y4;
-        }
-        for (auto e : group) {
-            auto it = find(dims.begin(), dims.end(), e);
-            if (it != dims.end()) dims.erase(it);
-        }
-        if (group.size() == 1) {
-            seps.push_back(group[0]);
-        } else {
-            allgroups.push_back(group);
+    auto tresh = calc_treshold();
+    auto p1 = new double[func->dim], p2 = new double[func->dim], p3 = new double[func->dim], p4 = new double[func->dim];
+    for (int i = 0; i < func->dim; i++) p1[i] = func->x_bound_min[i];
+    double p1f = eval(p1);
+    auto p2_vec = vector<double>(func->dim, 0);
+    for (int i = 0; i < func->dim; i++) {
+        for (int j = 0; j < func->dim; j++) p2[j] = p1[j];
+        p2[i] = func->x_bound_max[i];
+        p2_vec[i] = eval(p2);
+    }
+    auto p3_vec = vector<double>(func->dim, 0);
+    for (int i = 0; i < func->dim; i++) {
+        for (int j = 0; j < func->dim; j++) p3[j] = p1[j];
+        p3[i] = func->x_bound_min[i] + ((func->x_bound_max[i] - func->x_bound_min[i]) / 2);
+        p3_vec[i] = eval(p3);
+    }
+    auto deltaMtx = vector<vector<double>>(func->dim, vector<double>(func->dim, 0));
+    for (int i = 0; i < func->dim; i++) {
+        for (int j = 0; j < func->dim; j++) {
+            for (int k = 0; k < func->dim; k++) p4[k] = p1[k];
+            p4[i] = func->x_bound_max[i];
+            p4[j] = func->x_bound_min[i] + ((func->x_bound_max[i] - func->x_bound_min[i]) / 2);
+            auto p4f = eval(p4);
+            auto d_1 = p1f - p2_vec[i];
+            auto d_2 = p3_vec[j] - p4f;
+            deltaMtx[i][j] = abs(d_1 - d_2);
+            deltaMtx[j][i] = deltaMtx[i][j];
         }
     }
-    delete[] p1, delete[] p2;
+    delete [] p1, delete [] p2, delete [] p3, delete [] p4;
+    auto [labels, rst] = graph_connected_components(deltaMtx, tresh);
+    auto group_idx = labels;
+    for (int i = 0; i < *std::max_element(labels.begin(), labels.end()); i++) {
+        auto groupsize = std::count(labels.begin(), labels.end(), i);
+        if (groupsize == 1) {
+            for (int j = 0; j < labels.size(); j++) if (labels[j] == i) group_idx[labels[j]] = 0;
+            for (int j = 0; j < labels.size(); j++) if (labels[j] > i) group_idx[labels[j]] = group_idx[labels[j]] - 1;
+        }
+    }
+    auto seps = find<int, unsigned int>(group_idx, 0);
+    auto group_num = *std::max_element(group_idx.begin(), group_idx.end());
+    auto allgroups = vector<vector<unsigned int>>();
+    for (int i = 1; i < group_num; i++) allgroups.push_back(find<int, unsigned int>(group_idx, i));
     return make_tuple(seps, allgroups);
 }
 
-void GDG::graph_connected_components() {
-    // TODO
+tuple<vector<int>, vector<int>> GDG::graph_connected_components(vector<vector<double>> &C, const double trash) {
+    auto labels = vector<int>(func->dim, -1);
+    auto rts = vector<int>();
+    size_t ccc = 0;
+    while (true) {
+        auto ind = find<int, size_t>(labels, -1);
+        if (ind.size() == 0) {
+            auto fue = ind[0];
+            rts.push_back(fue);
+            auto list = vector<size_t>();
+            list.push_back(fue);
+            ccc++;
+            while (true) {
+                auto list_new = vector<size_t>();
+                for (int lc = 0; lc < list.size(); lc++) {
+                    auto p = list[lc];
+                    auto cp = find_tresh(C[p], trash);
+                    auto cp1 = vector<size_t>();
+                    for (auto i : cp) if (labels[i] == 0) cp1.push_back(i);
+                    for (auto i : cp1) labels[i] = ccc;
+                    for (auto e : cp1) list_new.push_back(e);
+                }
+                list = list_new;
+                if (list.size() == 0) return make_tuple(labels, rts);
+            }
+        } else {
+            return make_tuple(labels, rts);
+        }
+    }
 }
 
 double GDG::calc_treshold() {
@@ -59,4 +100,10 @@ double GDG::calc_treshold() {
     auto minf = abs(xf[0]);
     for (int i = 1; i < np; i++) if (minf > abs(xf[i])) minf = abs(xf[i]);
     return minf * epsilon;
+}
+
+vector<size_t> GDG::find_tresh(vector<double> &vec, double tresh) {
+    auto r = vector<size_t>();
+    for (int i = 0; i < vec.size(); i++) if (vec[i] <= tresh) r.push_back(i);
+    return r;
 }
--- gdg.hpp
+++ gdg.hpp
@@ -5,11 +5,12 @@
 
 class GDG: public AnalizeAlgorithm {
 public:
-    double epsilon;
     size_t np;
+    double epsilon;
+    double temp;
 
-    GDG() : GDG(10) {};
-    GDG(size_t np) : np(np) {};
+    GDG() : GDG(10, 10e-10) {};
+    GDG(size_t np, double epsilon) : np(np), epsilon(epsilon) {};
     ~GDG() {};
 
     virtual tuple<vector<unsigned int>, vector<vector<unsigned int>>> run(TestFuncBounds*) override;
@@ -17,9 +18,16 @@ public:
     virtual string sinfo() override;
 
 protected:
-    void graph_connected_components();
+    tuple<vector<int>, vector<int>> graph_connected_components(vector<vector<double>>&, double);
     double calc_treshold();
+    vector<size_t> find_tresh(vector<double> &, double);
 
+    template<typename T, typename R>
+    vector<R> find(vector<T> &vec, T val) {
+        auto r = vector<R>();
+        for (int i = 0; i < vec.size(); i++) if (vec[i] == val) r.push_back(i);
+        return r;
+    }
 };
 
 #endif
