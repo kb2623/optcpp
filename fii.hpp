12a61d1b19235de37558998de6cecfd15a07befe
Klemen <roxor1992@gmail.com>
2022-08-04 14:21:47 +0200

    Added some fixes

--- fii.hpp
+++ fii.hpp
@@ -14,13 +14,6 @@ public:
 	virtual string sinfo() override;
 	virtual void setParameters(AlgParams*) override;
 	virtual tuple<vector<unsigned int>, vector<vector<unsigned int>>> run(TestFuncBounds*) override;
-
-protected:
-	virtual vector<unsigned int> interact(double*, double, double, vector<unsigned int>, vector<unsigned int>, vector<unsigned int>&);
-	double calc_epsilon(TestFuncBounds* func);
-
-	size_t np;
-	double alpha;
 };
 
 #endif
